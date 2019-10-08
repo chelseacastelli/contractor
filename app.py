960c8bd507b3e0d95089e0d586b3535002f2d388
chelseacastelli <chelsea.castelli@students.makeschool.com>
2019-10-07 19:16:12 -0700

    Data shows

--- app.py
+++ app.py
@@ -2,10 +2,21 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# OUR MOCK ARRAY OF PROJECTS
+vinyls = [
+    { 'artist': 'Fleetwood Mac', 'album': 'Rumours', 'price': 24.98, 'image': './static/fleetwood_mac.jpeg' },
+    { 'artist': 'Hozier', 'album': 'Wasteland, Baby!', 'price': 25.66, 'image': './static/hozier.jpg' }
+]
+
 @app.route('/')
 def index():
     """Return homepage."""
-    return render_template('home.html', msg='Flask is Cool!!')
+    return render_template('index.html', vinyls=vinyls)
+
+@app.route('/vinyls')
+def store_index():
+    """Show all vinyls."""
+    return render_template('index.html', vinyls=vinyls)
 
 if __name__ == '__main__':
     app.run(debug=True)
