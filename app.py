5360258d990c98c4ea516cffb30074fa722f454a
chelseacastelli <chelsea.castelli@students.makeschool.com>
2019-10-07 19:29:14 -0700

    Add MongoDB and Vinyls model

--- app.py
+++ app.py
@@ -1,17 +1,22 @@
 from flask import Flask, render_template
+from pymongo import MongoClient
+
+client = MongoClient()
+db = client.Contractor
+vinyls = db.vinyls
 
 app = Flask(__name__)
 
 # OUR MOCK ARRAY OF PROJECTS
-vinyls = [
-    { 'artist': 'Fleetwood Mac', 'album': 'Rumours', 'price': 24.98, 'image': './static/fleetwood_mac.jpeg' },
-    { 'artist': 'Hozier', 'album': 'Wasteland, Baby!', 'price': 25.66, 'image': './static/hozier.jpg' }
-]
+# vinyls = [
+#     { 'artist': 'Fleetwood Mac', 'album': 'Rumours', 'price': 24.98, 'image': './static/fleetwood_mac.jpeg' },
+#     { 'artist': 'Hozier', 'album': 'Wasteland, Baby!', 'price': 25.66, 'image': './static/hozier.jpg' }
+# ]
 
 @app.route('/')
 def index():
     """Return homepage."""
-    return render_template('index.html', vinyls=vinyls)
+    return render_template('index.html', vinyls=vinyls.find())
 
 @app.route('/vinyls')
 def store_index():
