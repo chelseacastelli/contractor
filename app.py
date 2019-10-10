bc3f4c4b2416fdf1e9a22f23d524333cf71b735a
chelseacastelli <chelsea.castelli@students.makeschool.com>
2019-10-09 19:06:14 -0700

    Add production db

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from pymongo import MongoClient
 from bson.objectid import ObjectId
 import os
 
-
 host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/Contractor')
 client = MongoClient(host=f'{host}?retryWrites=false')
 db = client.get_default_database()
@@ -80,7 +79,7 @@ def remove_from_cart(cart_id):
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+  app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 5000))
 
 
 
